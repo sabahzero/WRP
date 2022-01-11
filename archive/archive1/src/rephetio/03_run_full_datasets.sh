320155868f8fc12c943b68b71ad27eb39a63570c
sabahzero <bysabahulhasan@gmail.com>
2022-01-11 14:11:34 -0800

    Updates to WRP folder

--- archive/archive1/src/rephetio/03_run_full_datasets.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# see .02 for parallel edits made
-
-LOAD_DIR='out/' # line adjusted from original
-
-for folder in ${LOAD_DIR}*
-    do python3 full_dataset_training.py $folder
-done
--- archive/archive1/src/rephetio/README.md
+++
@@ -1,6 +0,0 @@
-# Code
-
-This folder contains all the code needed to run this repo.
-
-All scripts and notebooks should be run in number order.
-
